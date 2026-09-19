dab2edc048b830accc677fa69ae2b7874ce30665
rajrai <rajrai913197@gmail.com>
2026-09-19 16:27:21 +0530

    Fix Cloudinary profile image upload

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,5 +1,5 @@
 const { v2: cloudinary } = require("cloudinary");
-
+require("dotenv").config()
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
--- controller/UserController.js
+++ controller/UserController.js
@@ -1,37 +1,103 @@
+const UserModel = require("../model/UserModel");
 
-const UserModel = require("../model/UserModel")
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
-const key = process.env.JWT_SECRET
-const createUser = async(req,res)=>{
-    const {name,email,password} = req.body
-    const hashpass = await bcrypt.hash(password,10)
-    const UserData = await UserModel.create({name , email,password:hashpass})
-    res.json(UserData)
-}
-const userLogin = async(req,res) =>{
-    const {email,password} = req.body
-       const findUser = await UserModel.findOne({email})
-       role = findUser.role
-      const comp = await bcrypt.compare(password,findUser.password)
-      if(!comp){
-         res.json("User Not Found")
-      } else{
-        const token =   jwt.sign({
-            userId : findUser._id,
-            role : role
-           
-           },key)
-           res.json({token : token,role : role})
-           console.log(role)
-      }
-}
+const key = process.env.JWT_SECRET;
+
+
+// ================= CREATE USER =================
+
+const createUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const hashpass = await bcrypt.hash(password, 10);
+
+    const UserData = await UserModel.create({
+      name,
+      email,
+      password: hashpass,
+    });
+
+    res.json(UserData);
+
+  } catch (error) {
+    console.log("CREATE USER ERROR:", error);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+
+// ================= LOGIN =================
+
+const userLogin = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+    const findUser = await UserModel.findOne({ email });
+
+    if (!findUser) {
+      return res.json("User Not Found");
+    }
+
+    const role = findUser.role;
+
+    const comp = await bcrypt.compare(
+      password,
+      findUser.password
+    );
+
+    if (!comp) {
+      return res.json("User Not Found");
+    }
+
+    const token = jwt.sign(
+      {
+        userId: findUser._id,
+        role: role,
+      },
+      key
+    );
+
+    res.json({
+      token: token,
+      role: role,
+    });
+
+    console.log("LOGIN ROLE:", role);
+
+  } catch (error) {
+
+    console.log("LOGIN ERROR:", error);
+
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+
+// ================= UPDATE PROFILE =================
 
 const userProfile = async (req, res) => {
-  const createUserData = await UserModel.findByIdAndUpdate(
-    req.params.id,
-    {
+
+  try {
+
+    console.log("PROFILE UPDATE ID:", req.params.id);
+
+    console.log("BODY:", req.body);
+
+    console.log("FILE:", req.file);
+
+
+    // ================= UPDATE DATA =================
+
+    const updateData = {
+
       name: req.body.name,
 
       age: req.body.age,
@@ -48,8 +114,6 @@ const userProfile = async (req, res) => {
 
       bio: req.body.bio,
 
-      image: req.file.path,
-
       fatherName: req.body.fatherName,
 
       motherName: req.body.motherName,
@@ -58,20 +122,125 @@ const userProfile = async (req, res) => {
 
       siblings: req.body.siblings,
 
-      profileComplete: true
-    },
-    { returnDocument : true}
-  );
+      profileComplete: true,
 
-  res.json(createUserData);
+    };
+
+
+    // ================= IMAGE =================
+
+    if (req.file) {
+
+      console.log("CLOUDINARY IMAGE:", req.file.path);
+
+      updateData.image = req.file.path;
+
+    }
+
+
+    // ================= DATABASE UPDATE =================
+
+    const createUserData =
+      await UserModel.findByIdAndUpdate(
+
+        req.params.id,
+
+        updateData,
+
+        {
+          new: true,
+        }
+
+      );
+
+
+    if (!createUserData) {
+
+      return res.status(404).json({
+        message: "User not found",
+      });
+
+    }
+
+
+    console.log(
+      "PROFILE UPDATED:",
+      createUserData
+    );
+
+
+    res.json(createUserData);
+
+
+  } catch (error) {
+
+    console.log(
+      "PROFILE UPDATE ERROR:",
+      error
+    );
+
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+
+  }
+
 };
 
-const getUserProfile = async(req,res)=>{
-   const userProfileData = await  UserModel.find()
-   res.json(userProfileData)
-}
-const getUserProfileDetail =async (req,res)=>{
-   const  userProfiledetaildata =  await UserModel.findById(req.params.id)
-   res.json(userProfiledetaildata)
-}
-module.exports = {createUser,userLogin,userProfile,getUserProfile,getUserProfileDetail} 
+
+// ================= GET ALL USERS =================
+
+const getUserProfile = async (req, res) => {
+
+  try {
+
+    const userProfileData =
+      await UserModel.find();
+
+    res.json(userProfileData);
+
+  } catch (error) {
+
+    console.log("GET USERS ERROR:", error);
+
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+
+  }
+
+};
+
+
+// ================= GET USER BY ID =================
+
+const getUserProfileDetail = async (req, res) => {
+
+  try {
+
+    const userProfiledetaildata =
+      await UserModel.findById(req.params.id);
+
+    res.json(userProfiledetaildata);
+
+  } catch (error) {
+
+    console.log("GET USER ERROR:", error);
+
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+
+  }
+
+};
+
+
+module.exports = {
+  createUser,
+  userLogin,
+  userProfile,
+  getUserProfile,
+  getUserProfileDetail,
+};
